52cd99ed6adc2668aae71e7e4f67f1a57d8f3b8c
kingsuper <neal.wang@foxmail.com>
2019-12-14 17:27:56 +0800

    update

--- leetcodejava/src/Solution752.java
+++ leetcodejava/src/Solution752.java
@@ -9,37 +9,42 @@ public class Solution752 {
         for (String s : deadends) {
             dead.add(s);
         }
+        if(dead.contains("0000")){
+            return -1;
+        }
         Set<String> seen = new HashSet<String>();
         Queue<String> queue = new LinkedList<String>();
         queue.offer("0000");
         seen.add("0000");
 
         int depth = 0;
-        while (!queue.isEmpty()){
-           String node = queue.poll();
-           depth++;
-
-           if(node.equals(target)){
-               return depth;
-           }
-          for(String s : getNexts(node)){
-              if (!dead.contains(s) && !seen.contains(s)){
-                  queue.offer(s);
-                  seen.add(s);
-              }
-          }
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                String node = queue.poll();
+                if (node.equals(target)) {
+                    return depth;
+                }
+                for (String s : getNexts(node)) {
+                    if (!dead.contains(s) && !seen.contains(s)) {
+                        queue.offer(s);
+                        seen.add(s);
+                    }
+                }
+            }
+            depth++;
         }
         return -1;
     }
 
-    private List<String> getNexts(String cur){
+    private List<String> getNexts(String cur) {
         List<String> list = new ArrayList<>();
-        for(int i =0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             StringBuilder curBuilder = new StringBuilder(cur);
-            char modeCar =cur.charAt(i);
-            curBuilder.setCharAt(i,modeCar=='0'?'9':(char)(modeCar-1));
+            char modeCar = cur.charAt(i);
+            curBuilder.setCharAt(i, modeCar == '0' ? '9' : (char) (modeCar - 1));
             list.add(curBuilder.toString());
-            curBuilder.setCharAt(i,modeCar=='9'?'0':(char)(modeCar+1));
+            curBuilder.setCharAt(i, modeCar == '9' ? '0' : (char) (modeCar + 1));
             list.add(curBuilder.toString());
         }
         return list;
