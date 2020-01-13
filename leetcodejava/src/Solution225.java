a9306caf1ef0a94cae929471ffe7a17bc7cedcc3
kingsuper <neal.wang@foxmail.com>
2020-01-13 19:50:38 +0800

    modify

---
+++ leetcodejava/src/Solution225.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+ */
+
+import java.util.Stack;
+
+/**
+ * 功能描述
+ *
+ * @author w00468379
+ * @since 2020-01-12
+ */
+class Solution225 {
+
+    Stack<Integer> stack;
+    /**
+     * Initialize your data structure here.
+     */
+    public Solution225() {
+        this.stack = new Stack<>();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        stack.
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+
+    }
+
+}
---
+++ leetcodejava/src/Solution94.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 功能描述
+ *
+ * @author w00468379
+ * @since 2020-01-11
+ */
+class Solution94 {
+/*    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        help(root, res);
+        return res;
+    }
+
+    public void help(TreeNode root, List res) {
+        if (root != null) {
+            if (root.left != null) {
+                help(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                help(root.right, res);
+            }
+        }
+    }*/
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        return res;
+    }
+}
+
+
+
